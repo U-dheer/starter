5933a6adb810b3d1f75f4fa295f38ca3f7bc67e0
U-dheer <udithadheerendra10@gmail.com>
2026-02-13 18:58:48 +0530

    feat: update zudoku configuration to use new sidebar structure and improve navigation links

--- natours/zudoku.config.tsx
+++ natours/zudoku.config.tsx
@@ -1,63 +1,58 @@
 import type { ZudokuConfig } from "zudoku";
 
 const config: ZudokuConfig = {
-  site: {
-    title: "Natours API",
+  page: {
+    pageTitle: "Natours API",
     logo: {
       src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
       alt: "Natours",
       width: "130px",
     },
   },
-  navigation: [
-    {
-      type: "category",
-      label: "Documentation",
-      items: [
-        {
-          type: "category",
-          label: "Getting Started",
-          icon: "sparkles",
-          items: [
-            "/introduction",
-            "/authentication",
-          ],
-        },
-        {
-          type: "link",
-          to: "/api",
-          label: "API Reference",
-          icon: "book-open",
-        },
-        {
-          type: "category",
-          label: "Useful Links",
-          collapsible: false,
-          icon: "link",
-          items: [
-            {
-              type: "link",
-              icon: "external-link",
-              label: "Live Swagger UI",
-              to: "http://localhost:3000/api-docs",
-            },
-            {
-              type: "link",
-              icon: "book",
-              label: "Zudoku Docs",
-              to: "https://zudoku.dev/docs/",
-            },
-          ],
-        },
-      ],
-    },
-  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Documentation",
+        items: [
+          {
+            type: "category",
+            label: "Getting Started",
+            icon: "sparkles",
+            items: ["/introduction", "/authentication"],
+          },
+          {
+            type: "link",
+            href: "/api",
+            label: "API Reference",
+          },
+          {
+            type: "category",
+            label: "Useful Links",
+            collapsible: false,
+            icon: "link",
+            items: [
+              {
+                type: "link",
+                label: "Live Swagger UI",
+                href: "https://natours-rho-self.vercel.app/api-docs",
+              },
+              {
+                type: "link",
+                label: "Zudoku Docs",
+                href: "https://zudoku.dev/docs/",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
   redirects: [{ from: "/", to: "/introduction" }],
   apis: [
     {
       type: "file",
       input: "./apis/openapi.json",
-      path: "/api",
     },
   ],
 };
